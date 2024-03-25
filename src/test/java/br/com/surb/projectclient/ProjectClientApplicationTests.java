8623ce467686d4acc8235ebebd5832d93c524cc2
claudiobelempb <claudio.c.lima@hotmail.com>
2024-03-25 08:11:12 -0300

    first commit

---
+++ src/test/java/br/com/surb/projectclient/ProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surb.projectclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
